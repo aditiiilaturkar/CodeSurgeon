54639dc77208b3efa29255291fa6b96d8b2df8a4
aditiiilaturkar <aditi.laturkar@gmail.com>
2023-10-23 20:37:52 -0400

    Updated python script

--- main.py
+++ main.py
@@ -1,82 +1,64 @@
 import subprocess
 import re
 
-def get_line_numbers(filename, pattern):
-    command = f"grep -n {pattern} {filename} | cut -d: -f1"
-    output = subprocess.check_output(command, shell=True, universal_newlines=True)
-    lines = output.strip().splitlines()
-    return lines
+COMMAND_COMPILE = "g++ -O0 -g main.cpp -o my_cpp_executable"
+COMMAND_PERF_RECORD = "sudo perf record -e intel_pt//u -- ./my_cpp_executable C"
+COMMAND_MMAP = "sudo perf script --show-mmap-events | grep MMAP | grep my_cpp_executable"
+COMMAND_INSN_TRACE = "sudo perf script --insn-trace"
+COMMAND_ADDR2LINE = "addr2line -e my_cpp_executable "
 
-def convert_address_to_line(base_addr, address):
-    bash_command = f'echo $(( {address} - {base_addr} ))'
-    process = subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE)
-    output, _ = process.communicate()
-    addr = output.decode('utf-8').strip()
-    addr = hex(int(addr))
-    addr = "1" +  addr[2:]  
-    command = f'addr2line -e my_cpp_executable {addr}'
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    cur_result = output.decode('utf-8').strip()
-    return cur_result
- 
 
-def get_insn_trace_analysis():
-    # perf_record_command = "sudo perf record -e intel_pt//u -- ./my_cpp_executable"
-    # process = subprocess.Popen(perf_record_command, shell=True)
-    # process.communicate()
-
-    mmap_addr_command = "sudo perf script --show-mmap-events | grep MMAP | grep my_cpp_executable"
-    output = subprocess.check_output(mmap_addr_command, shell=True, universal_newlines=True)
-    lines = output.splitlines()
-    if lines:
-        mmap_addr = lines[0]
+def exec_command(command, capture_output=False):
+    # print(f"\n=========== Executing [[ {command} ]] ===========")
+    if capture_output:
+        output = subprocess.check_output(command, shell=True, universal_newlines=True)
+        lines = output.splitlines()
+        return lines
     else:
-        mmap_addr = None
-
-    print(f"mmap_addr: {mmap_addr}")
-
-    size = re.search(r'\((.*?)\)', mmap_addr).group(1)
-    print(f"size: {size}")
-
-    base_addr = mmap_addr.split('[')[2]
-    base_addr = base_addr.split('(')[0]
-    print(f"base_addr: {base_addr}")
-
-    base_addr_pre = base_addr[2:10]
-    print(f"base_addr_pre: {base_addr_pre}")
-
-    insn_trace_command = f"sudo perf script --insn-trace | grep {base_addr_pre} > temp1.txt"
-    process = subprocess.Popen(insn_trace_command, shell=True)
-    process.communicate()
-
-    filename = "temp1.txt"
-    pattern = "main+0x"
-    line_numbers = get_line_numbers(filename, pattern)
-    if len(line_numbers) >= 2:
-        lineStart = line_numbers[0]
-        lineEnd = line_numbers[-2]
-
-        print(f"lineStart: {lineStart}")
-        print(f"lineEnd: {lineEnd}")
-    else:
-        print("Pattern not found in the file.")
-    
-    process_lines_command = f"awk -v f={lineStart} -v l={lineEnd} 'NR>=f && NR<=l' temp1.txt | awk '{{print \"0x\"$5}}' > temp2.txt"
-    subprocess.Popen(process_lines_command, shell=True)
-
-    prev_result = ""
-    with open("temp2.txt", "r") as file:
-        for line in file:
-            address = line.strip()
-            cur_result = convert_address_to_line(base_addr, address)
-            if cur_result != prev_result:
-                print(cur_result)
-
-            prev_result = cur_result
-
+        process = subprocess.Popen(command, shell=True)
+        process.communicate()
 
 
 if __name__ == "__main__":
-    subprocess.run(["g++", "-O0", "-g", "main.cpp", "-o", "my_cpp_executable"])
-    get_insn_trace_analysis()
+    # ============================ STEP 1 ============================
+    exec_command(COMMAND_COMPILE)
+
+    # ============================ STEP 2 ============================
+    exec_command(COMMAND_PERF_RECORD)
+
+    # ============================ STEP 3 ============================
+    output_lines = exec_command(COMMAND_MMAP, capture_output=True)
+    mmap_addr = output_lines[0]
+
+    size = re.search(r'\((.*?)\)', mmap_addr).group(1)
+
+    base_addr = mmap_addr.split('[')[2]
+    base_addr = base_addr.split('(')[0]
+
+    base_addr_pre = base_addr[2:10]
+    print(f"[mmap_addr: {mmap_addr}]\n[size: {size}]\n[base_addr: {base_addr}]\n[base_addr_pre: {base_addr_pre}]")
+
+    # ============================ STEP 4 ============================
+    insn_trace_output = exec_command(COMMAND_INSN_TRACE + f" | grep {base_addr_pre}", capture_output=True)
+    idx_start = -1
+    idx_end = -1
+
+    for idx, line in enumerate(insn_trace_output):
+        if "main+0x" in line:
+            if idx_start == -1:
+                idx_start = idx
+            idx_end = idx
+
+    prev_result = ""
+    for idx in range(idx_start, idx_end):
+        line = insn_trace_output[idx]
+        data = line.strip().split()
+        addr = '0x' + data[4]
+        diff = hex(int(addr, 16) - int(base_addr, 16))
+        addr = '1' + diff[2:]
+        out = exec_command(COMMAND_ADDR2LINE + addr, capture_output=True)
+        cur_result = out[0]
+        if cur_result != prev_result:
+            print(cur_result)
+
+        prev_result = cur_result
